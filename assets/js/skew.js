f73a8c97e43818f9014a92b0dcac3fa8a6d1d3d0
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-03-14 10:51:12 +0100

    Den færdig Opgave

---
+++ assets/js/skew.js
@@ -0,0 +1,26 @@
+//Skriv to globale variabler. 
+//Den første hedder xAnswer og finder DOM elementet med id'et "xAnswer".
+//Den anden hedder inputX og finder DOM elementet med id'et "x".
+
+
+//Skriv to globale variabler. 
+//Den første hedder yAnswer og finder DOM elementet med id'et "yAnswer".
+//Den anden hedder inputY og finder DOM elementet med id'et "y".
+
+
+//Skriv den globale variabel som hedder img og finder DOM elementet img.
+
+
+//Lav to gobale variabler der hedder xvalue og yvalue, som gemmer x- og y-aksens værdier. De skal sættes til 0.
+
+
+//Lav en eventListener for inputX, som "lytter" efter input. 
+//Sæt xvalue til at være lig med inputX value (dette kan du gøre med event.target.value).
+//BONUS: Hvis du vil kunne se hvor mange grader billedet bliver "skubbet" skal du sætte innerText i xAnswer til at være lig med xvalue.
+//Sæt img's style til at lig med transform: skew. værdierne i skew sættes til xvalue og yvalue. HINT: brug en literal string : `transform: skew(${ }deg, ${ }deg);`.
+
+
+//Gentag samme eventListener bare for inputX
+
+
+
